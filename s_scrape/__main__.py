975eee2a2fca506b004a8f02756855eb170b2397
berkkarahan <berkkarahan00@gmail.com>
2018-07-26 23:57:27 +0300

    Update __main__.py

--- s_scrape/__main__.py
+++ s_scrape/__main__.py
@@ -2,32 +2,7 @@ import time
 from s_scrape.scraping import DetailsScraper, MainPageScraper
 from s_scrape.utils import IO
 
-def _mainruntime(njobs=4,method_submodel='runtime', chunkify = True, wait = 10):
-
-    mscr = MainPageScraper(njobs)
-    print("Scraping started...")
-    mscr.scrapeModels()
-    print("Main car models scraped...")
-    mscr.scrapeSubModels(method = method_submodel)
-    print("Sub car models scraped...")
-    print("Waiting %d seconds before scraping listings..." %wait)
-    time.sleep(wait)
-    if chunkify:
-        listings = mscr.batch_scrapeListings()
-    else:
-        listings = mscr.scrapeListings()
-    IO.save_list("listings.txt",listings)
-    scr = DetailsScraper(listings, njobs)
-    print("Waiting %d seconds before scraping listings..." %wait)
-    time.sleep(wait)
-    if chunkify:
-        scr.batch_scrapeDetails()
-    else:
-        scr.scrapeDetails()
-    return scr.final_list
-
-
-def _threading(njobs=4, wait = 10):
+def _main(njobs=4, wait = 10):
     print("Total threads: %s"%str(njobs))
     mscr = MainPageScraper(njobs)
     print("Scraping started...")
@@ -47,7 +22,7 @@ def _threading(njobs=4, wait = 10):
 
 if __name__ == "__main__":
 
-    results = _threading(njobs=128, wait = 10)
+    results = _main(njobs=128, wait = 10)
 
     try:
         print("Using pandas for easier CSV extraction...")
