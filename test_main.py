63c7008acc063b6b98ea8b6011eddb48082e69af
Berk Karahan <berkkarahan@pop-os.localdomain>
2018-07-26 23:55:33 +0300

    small fix

--- test_main.py
+++ test_main.py
@@ -1,6 +1,3 @@
-import time
-import itertools
-import numpy
 from s_scrape.scraping import DetailsScraper, MainPageScraper
 from s_scrape.utils import IO
 
@@ -8,17 +5,17 @@ listingswait = 5
 mainwait = 15
 
 #print("Currently loading listings from pre-scraped list...")
-mscr = MainPageScraper(8)
+mscr = MainPageScraper(256)
 print("Scraping started...")
 mscr.scrapeModels()
 print("Main car models scraped...")
-mscr.scrapeSubModels(method='test')
+mscr.scrapeSubModels()
 print("Sub car models scraped...")
 print("Waiting %d seconds before scraping listings..." %listingswait)
 time.sleep(listingswait)
-listings = mscr.scrapeListings(method='test')
-IO.save_list("listings.txt",listings)
-scr = DetailsScraper(listings, 8)
+mscr.scrapeListings()
+IO.save_list("listings.txt",mscr.listings)
+scr = DetailsScraper(listings, 64)
 print("Waiting %d seconds before scraping listings..." %mainwait)
 time.sleep(mainwait)
 scr.scrapeDetails(method='test')
