2cf0847e91c4d3608434cd49982ef82be5f32cfc
Berk Karahan <berkkarahan00@gmail.com>
2019-02-02 16:08:27 +0300

    re-introduce apis for requesting

---
+++ test_api.py
@@ -0,0 +1,15 @@
+from s_scrape.base import URLrequests
+from s_scrape.srequests import URLlib
+import requests
+
+api = URLrequests()
+
+url = 'https://stackoverflow.com/questions/627435/how-do-i-remove-an-element-from-a-list-by-index-in-python'
+
+api.apireadURL(url)
+
+u = URLlib()
+
+u.readURL(url)
+
+requests.get('https://www.photopea.com/mirror.php?url=' + url, verify=False).text
